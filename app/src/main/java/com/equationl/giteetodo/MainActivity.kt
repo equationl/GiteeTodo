3bc5ad4cb1850d7b27f5a4e34f0925c77db4e77b
equationl <equationl@qq.com>
2022-05-10 10:48:22 +0800

    fixed #I56D8N

--- app/src/main/java/com/equationl/giteetodo/MainActivity.kt
+++ app/src/main/java/com/equationl/giteetodo/MainActivity.kt
@@ -7,19 +7,27 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.lifecycleScope
 import com.equationl.giteetodo.ui.HomeNavHost
 import com.equationl.giteetodo.ui.theme.GiteeTodoTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            GiteeTodoTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    HomeNavHost()
+        lifecycleScope.launch {
+            // for issue: https://gitee.com/equation/GiteeTodo/issues/I56D8N
+            // see this: https://v2ex.com/t/851784
+            delay(100)
+            setContent {
+                GiteeTodoTheme {
+                    Surface(
+                        modifier = Modifier.fillMaxSize(),
+                        color = MaterialTheme.colors.background
+                    ) {
+                        HomeNavHost()
+                    }
                 }
             }
         }
